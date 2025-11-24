8e9fc6f9717c22dec705275e026c5e4d636d50a0
JangJuMan <wnaks9901@gmail.com>
2025-11-24 12:49:53 +0900

    [Day 111] Daily Codekata(~25/11/24) - 복습
    91. 기능개발(ceil 구현, vec.back()) / RE(완료)

--- 091.cpp
+++ 091.cpp
@@ -1,26 +1,57 @@
-// 91. 기능개발(ceil 구현, vec.back()) / RE
+// 91. 기능개발(ceil 구현, vec.back()) / RE(완료)
 // https://school.programmers.co.kr/learn/courses/30/lessons/42586
+#include <string>
 #include <vector>
+#include <queue>
 #include <cmath>
 
 using namespace std;
 
-vector<int> solution(vector<int> progresses, vector<int> speeds){
-    vector<int> ans;
-    int day=0, max_day=0;
-    const float DONE = 100.0f;
+
+/* 2회차 풀이. 근데 저번에 풀었던게 더 좋은듯? */
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    queue<int> q;
     for(int i=0; i<progresses.size(); i++){
-        day = static_cast<int>(ceil((DONE - progresses[i])/speeds[i]));
-        if(max_day < day){
-            ans.push_back(1);
-            max_day = day;
+        int remain = 100 - progresses[i];
+        int day = remain / speeds[i];
+        if(remain % speeds[i] > 0){
+            day++;
         }
-        else{
-            ++ans.back();
+        q.emplace(day);
+    }
+    
+    vector<int> ans;
+    while(!q.empty()){
+        int cur = q.front();
+        int res = 1;
+        q.pop();
+        while(!q.empty() && cur >= q.front()){
+            q.pop();
+            res++;
         }
+        ans.emplace_back(res);
     }
     return ans;
 }
+
+
+/* 1회차 풀이 */
+// vector<int> solution(vector<int> progresses, vector<int> speeds){
+//     vector<int> ans;
+//     int day=0, max_day=0;
+//     const float DONE = 100.0f;
+//     for(int i=0; i<progresses.size(); i++){
+//         day = static_cast<int>(ceil((DONE - progresses[i])/speeds[i]));
+//         if(max_day < day){
+//             ans.push_back(1);
+//             max_day = day;
+//         }
+//         else{
+//             ++ans.back();
+//         }
+//     }
+//     return ans;
+// }
 
 
 // #include <string>
