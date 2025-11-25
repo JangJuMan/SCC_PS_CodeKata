fc195053187ecd2a1d75998f263bc961da0e30ec
JangJuMan <wnaks9901@gmail.com>
2025-11-25 12:33:32 +0900

    [Day 112] Daily Codekata(~25/11/25) - 복습
    96. 주차 요금 계산 / 구현(re/re)

--- 096.cpp
+++ 096.cpp
@@ -1,4 +1,4 @@
-// 96. 주차 요금 계산(re) / 구현
+// 96. 주차 요금 계산 / 구현(re/re)
 // https://school.programmers.co.kr/learn/courses/30/lessons/92341
 #include <string>
 #include <vector>
@@ -8,41 +8,41 @@
 
 using namespace std;
 
-int TimeToMin(const string& time){
+int time_to_int(const string& time){    // "12:38" 형태
     return stoi(time.substr(0, 2)) * 60 + stoi(time.substr(3, 2));
 }
 
 vector<int> solution(vector<int> fees, vector<string> records) {
-    map<string, int> InTime;
-    map<string, int> TotalParkingTime;
+    map<string, int> entrance_time;
+    map<string, int> total_parking_time;
     
-    for(const auto& record : records){
-        stringstream SS(record);
-        string Time, Car, Type;
-        SS >> Time >> Car >> Type;
-        
-        if(Type == "IN"){
-            InTime[Car] = TimeToMin(Time);
+    for(const string& record : records){
+        stringstream ss(record);
+        string time, car, type;
+        ss >> time >> car >> type;
+        if(type == "IN"){
+            entrance_time[car] = time_to_int(time);
         }
-        else{
-            int In = InTime[Car];
-            int Out = TimeToMin(Time);
-            TotalParkingTime[Car] += Out - In;
-            InTime.erase(Car);
+        else if(type == "OUT"){
+            int in = entrance_time[car];
+            int out = time_to_int(time);
+            int diff = out - in;
+            total_parking_time[car] += diff;
+            entrance_time.erase(car);
         }
     }
     
-    for(const auto& [Car, In] : InTime){
-        TotalParkingTime[Car] += TimeToMin("23:59") - In;
+    for(const auto& [car, in_time] : entrance_time){
+        total_parking_time[car] += time_to_int("23:59") - in_time;
     }
     
     vector<int> ans;
-    for(const auto& [Car, T] : TotalParkingTime){
-        int fee = fees[1];
-        if(T > fees[0]){
-            fee += ceil((T - fees[0]) / (double)fees[2]) * fees[3];
+    for(const auto& [car, time] : total_parking_time){
+        int fee = fees[1];  // 기본요금
+        if(time > fees[0]){ // 기본시간보다 초과해서 주차하면 추가금
+            fee += ceil((time - fees[0]) / (double)fees[2]) * fees[3];
         }
-        ans.push_back(fee);
+        ans.emplace_back(fee);
     }
     return ans;
 }
