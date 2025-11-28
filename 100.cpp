5b8262c51444e08aa13aac1523894c80485a8439
JangJuMan <wnaks9901@gmail.com>
2025-11-28 12:04:49 +0900

    [Day 115] Daily Codekata(~25/11/28) - 복습
    100. 숫자 변환하기(dfs X, bfs O, dp) / re(완료)

--- 100.cpp
+++ 100.cpp
@@ -1,11 +1,27 @@
-// 100. 숫자 변환하기(dfs X, bfs O) : 최단경로 / re
+// 100. 숫자 변환하기(dfs X, bfs O, dp) / re(완료)
 // https://school.programmers.co.kr/learn/courses/30/lessons/154538
 #include <string>
 #include <vector>
 #include <queue>
+#include <climits>
 
 using namespace std;
 
+/* 2트 : dp로 풀기 */
+int solution(int x, int y, int n) {
+    vector<int> dp(y+1, INT_MAX);
+    
+    dp[x] = 0;
+    for(int i=x; i<=y; ++i){
+        if(dp[i] == INT_MAX) continue;
+        if(i+n <= y) dp[i+n] = min(dp[i+n], dp[i]+1);
+        if(i*2 <= y) dp[i*2] = min(dp[i*2], dp[i]+1);
+        if(i*3 <= y) dp[i*3] = min(dp[i*3], dp[i]+1);
+    }
+    return (dp[y] == INT_MAX) ? -1 : dp[y]; 
+}
+
+/* 1트 : bfs로 풀기 */
 int solution(int x, int y, int n) {
     vector<int> dist(y+1, -1);
     queue<int> q;
