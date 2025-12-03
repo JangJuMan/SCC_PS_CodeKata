2d4b3ebfad1dda1b14cba16e2707a6f6e47da122
JangJuMan <wnaks9901@gmail.com>
2025-12-03 12:07:25 +0900

    [Day 117] Daily Codekata(~25/12/03) - 복습
    102. 다리를 지나는 트럭(큐, 시뮬레이션) / re(re)

--- 102.cpp
+++ 102.cpp
@@ -1,4 +1,4 @@
-// 102. 다리를 지나는 트럭(큐) / re
+// 102. 다리를 지나는 트럭(큐, 시뮬레이션) / re(re)
 // https://school.programmers.co.kr/learn/courses/30/lessons/42583#
 #include <string>
 #include <vector>
@@ -6,29 +6,57 @@
 
 using namespace std;
 
+/* 2트 실패 */
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    queue<int> bridge;
-    int ans = 0, bridge_weight=0, idx=0;
+    queue<int> bridge_q;
+    int ans=0, idx=0, n=truck_weights.size(), bridge_weight=0;
     
-    while(idx < truck_weights.size()){
-        ans++;
+    while(idx < n){
+        ++ans;
         
-        if(bridge.size() == bridge_length){     // 큐를 0으로 초기화하지 않고, 꽉 차면 pop
-            bridge_weight -= bridge.front();
-            bridge.pop();
+        if(bridge_q.size() == bridge_length){               // 다리가 꽉 찬 경우, 가장 먼저 들어간 트럭(or 0) pop
+            bridge_weight -= bridge_q.front();
+            bridge_q.pop();
         }
         
-        if(bridge_weight + truck_weights[idx] <= weight){
-            bridge.push(truck_weights[idx]);
+        if(bridge_weight + truck_weights[idx] <= weight){   // 트럭이 올라갈 수 있다면 트럭 올리기
+            bridge_q.emplace(truck_weights[idx]);
             bridge_weight += truck_weights[idx];
-            idx++;
+            ++idx;
         }
-        else{
-            bridge.push(0);
+        else{       // 트럭이 못올라간다면 0 올리기
+            bridge_q.emplace(0);
         }
     }
+    
     return ans + bridge_length;
 }
+
+
+// int solution(int bridge_length, int weight, vector<int> truck_weights) {
+//     queue<int> bridge;
+//     int ans = 0, bridge_weight=0, idx=0;
+    
+//     while(idx < truck_weights.size()){
+//         ans++;
+        
+//         if(bridge.size() == bridge_length){     // 큐를 0으로 초기화하지 않고, 꽉 차면 pop
+//             bridge_weight -= bridge.front();
+//             bridge.pop();
+//         }
+        
+//         if(bridge_weight + truck_weights[idx] <= weight){
+//             bridge.push(truck_weights[idx]);
+//             bridge_weight += truck_weights[idx];
+//             idx++;
+//         }
+//         else{
+//             bridge.push(0);
+//         }
+//     }
+//     return ans + bridge_length;
+// }
+
 
 // int solution(int bridge_length, int weight, vector<int> truck_weights) {
 //     queue<int> q;
