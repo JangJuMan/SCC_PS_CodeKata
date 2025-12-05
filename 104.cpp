9993ae131a22a8467b58c6d7d2932230ad873039
JangJuMan <wnaks9901@gmail.com>
2025-12-05 12:40:24 +0900

    [Day 119] Daily Codekata(~25/12/05) - 복습
    104. 소수찾기(next_permutation, 6k+5, 6k+1) / re(re)

--- 104.cpp
+++ 104.cpp
@@ -1,4 +1,4 @@
-// 104. 소수찾기(next_permutation, 6k+5, 6k+1) / re
+// 104. 소수찾기(next_permutation, 6k+5, 6k+1) / re(re)
 // https://school.programmers.co.kr/learn/courses/30/lessons/42839
 #include <string>
 #include <vector>
@@ -7,7 +7,14 @@
 
 using namespace std;
 
-bool isPrime(int n){
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+bool isPrime(int n){       // 1번 방법과 동일
     if(n <= 1) return false;
     if(n <= 3) return true;
     if(n % 2 == 0 || n % 3 == 0) return false;
@@ -19,15 +26,13 @@ bool isPrime(int n){
 }
 
 int solution(string numbers) { 
-    unordered_set<int> res;
+    set<int> res;
     sort(numbers.begin(), numbers.end());
-    int len = numbers.size();
     
     do{
-        for(int i = 1; i <= len; i++){
+        for(int i = 1; i <= numbers.size(); i++){
             string part = numbers.substr(0, i);
-            int num = stoi(part);
-            res.insert(num);
+            res.insert(stoi(part));
         }
     }while(next_permutation(numbers.begin(), numbers.end()));
     
@@ -38,35 +43,46 @@ int solution(string numbers) {
     return ans;
 }
 
-// bool isVisited[8] = {false};
-// string inputStr = "";
-// int ans = 0;
-// unordered_set<int> hashSet;
+// bool visited[8];
+// string inputNum;
+// set<int> nums;
 
-// void dfs(int targetDepth, int depth, string res){
-//     if(depth == targetDepth){
+// bool isPrime(int n){
+//     if(n <= 1) return false;
+//     if(n <= 3) return true;
+//     if(n%2 == 0 || n%3 == 0) return false;
+    
+//     for(int i=5; i*i <= n; i+=6){
+//         if(n % i == 0 || n%(i+2) == 0) return false;
+//     }
+//     return true;
+// }
+
+// void dfs(int targetDepth, int cur, string res){
+//     if(cur == targetDepth){
 //         int val = stoi(res);
-//         if(hashSet.count(val)) return;
-//         hashSet.insert(val);
-//         if(isPrime(val)) ans++;
+//         nums.insert(val);
 //         return;
 //     }
     
-//     for(int i=0; i<inputStr.size(); i++){
-//         if(!isVisited[i]){
-//             isVisited[i] = true;
-//             dfs(targetDepth, depth+1, res + inputStr[i]);
-//             isVisited[i] = false;
+//     for(int i=0; i<inputNum.size(); i++){
+//         if(!visited[i]){
+//             visited[i] = true;
+//             dfs(targetDepth, cur+1, res+inputNum[i]);
+//             visited[i] = false;
 //         }
 //     }
 // }
 
-// int solution(string numbers) { 
-//     inputStr = numbers;
-    
+// int solution(string numbers) {
+//     inputNum = numbers;
 //     for(int i=1; i<=numbers.size(); i++){
 //         dfs(i, 0, "");
 //     }
     
+//     int ans = 0;
+//     for(int n : nums){
+//         if(isPrime(n)) ans++;
+//     }
 //     return ans;
 // }
