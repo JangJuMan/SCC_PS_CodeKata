b6f5a29fb6ad9beb97a5f93c545a3ff20e304b8d
JangJuMan <wnaks9901@gmail.com>
2025-12-09 11:50:10 +0900

    [Day 121] Daily Codekata(~25/12/09) - 복습
    106. 택배상자(스택) / re(re..?코드안이쁨)

--- 106.cpp
+++ 106.cpp
@@ -1,4 +1,4 @@
-// 106. 택배상자(스택) / re
+// 106. 택배상자(스택) / re(re..?코드안이쁨)
 // https://school.programmers.co.kr/learn/courses/30/lessons/131704
 #include <string>
 #include <vector>
@@ -6,17 +6,43 @@
 
 using namespace std;
 
+/* 2트.. 더 못생기게 풀었네.. 다시 풀어야하나? */
 int solution(vector<int> order) {
-    int idx=0;
+    int ans=0, boxIdx=1, orderIdx=0;
+    stack<int> st;
+    for(int boxIdx=1; boxIdx<=order.size(); boxIdx++){
+        if(boxIdx == order[orderIdx]){
+            ++ans;
+            ++orderIdx;
+            continue;
+        }
+        
+        while(!st.empty() && st.top() == order[orderIdx]){
+            ++ans;
+            ++orderIdx;
+            st.pop();
+        }
+        st.emplace(boxIdx);
+    }
+    
+    while(!st.empty() && st.top() == order[orderIdx]){
+        ++ans;
+        ++orderIdx;
+        st.pop();
+    }
+    return ans;   
+}
+
+int solution(vector<int> order) {
+    int orderIdx=0;
     stack<int> st;
     
-    for(int i=1; i<=order.size(); i++){
-        st.emplace(i);
-        
-        while(!st.empty() && st.top() == order[idx]){
+    for(int boxIdx=1; boxIdx<=order.size(); boxIdx++){
+        st.emplace(boxIdx);
+        while(!st.empty() && st.top() == order[orderIdx]){
             st.pop();
-            idx++;
+            ++orderIdx;
         }
     }
-    return idx;
+    return orderIdx;
 }
