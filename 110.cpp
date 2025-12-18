86b90fafe8833850266366b9ed45edc3637f487f
JangJuMan <wnaks9901@gmail.com>
2025-12-18 11:13:53 +0900

    [Day 125] Daily Codekata(~25/12/18) - 복습
    110. 두 큐 합 같게 만들기(투포인터) / re(풀긴함)

--- 110.cpp
+++ 110.cpp
@@ -1,10 +1,44 @@
-// 110. 두 큐 합 같게 만들기(투포인터) / re
+// 110. 두 큐 합 같게 만들기(투포인터) / re(풀긴함)
 // https://school.programmers.co.kr/learn/courses/30/lessons/118667
 #include <vector>
 #include <string>
 
 using namespace std;
 
+/* 2트 : 시뮬레이션 */
+int solution(vector<int> queue1, vector<int> queue2) {
+    int n = queue1.size();
+    long long sum_1 = 0, sum_2 = 0;
+    queue<int> q1, q2;
+    for(int i=0; i<n; ++i){
+        q1.emplace(queue1[i]);
+        sum_1 += queue1[i];
+        q2.emplace(queue2[i]);
+        sum_2 += queue2[i];
+    }
+    
+    long long target_sum = (sum_1 + sum_2) / 2;
+    for(int ans=0; ans<3*n; ++ans){
+        if(sum_1 == target_sum){
+            return ans;
+        }
+        else if(sum_1 < target_sum){
+            q1.emplace(q2.front());
+            sum_1 += q2.front();
+            sum_2 -= q2.front();
+            q2.pop();
+        }
+        else if(sum_1 > target_sum){
+            q2.emplace(q1.front());
+            sum_2 += q1.front();
+            sum_1 -= q1.front();
+            q1.pop();
+        }
+    }
+    return -1;
+}
+
+/* 1트 : 투포인터 */
 int solution(vector<int> queue1, vector<int> queue2){
     long long sum1 = 0, sum2 = 0;
     for(int i : queue1) sum1 += i;
