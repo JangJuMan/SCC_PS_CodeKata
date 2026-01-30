6139766c4c821283e02cbe11fa52331d196ea1fa
JangJuMan <wnaks9901@gmail.com>
2026-01-30 15:44:55 +0900

    [Day 127] Daily Codekata(~26/01/30) - 복습
     112. 행렬 테두리 회전하기 / 구현 / 풀었지만 re? (2트 완료)
    
    https://blog.naver.com/letterb926/224165447617

--- 112.cpp
+++ 112.cpp
@@ -1,4 +1,4 @@
-// 112. 행렬 테두리 회전하기 / 풀었지만 re?
+// 112. 행렬 테두리 회전하기 / 풀었지만 re? (2트 완료)
 // https://school.programmers.co.kr/learn/courses/30/lessons/77485
 #include <vector>
 #include <string>
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+/* 2트 했지만, 이 코드가 가독성이 좋음 */
 vector<int> solution(int rows, int columns, vector<vector<int>> queries){
     vector<vector<int>> board(rows, vector<int>(columns));
     int num = 1;
@@ -14,7 +15,7 @@ vector<int> solution(int rows, int columns, vector<vector<int>> queries){
             board[i][j] = num++;
 
     vector<int> ans;
-    for(auto& q : queries){ // vector<int>
+    for(const auto& q : queries){ // vector<int>  // [x1, y1, x2, y2] 로 하려면 vector가 아닌 고정 배열(array) 이어야 함.
         int r1 = q[0]-1, c1 = q[1]-1, r2 = q[2]-1, c2 = q[3]-1;
         int prev = board[r1][c1];
         int mn = prev;
