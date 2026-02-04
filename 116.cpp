a5f11743dfde1b4c7cac89580312613c7d075c79
JangJuMan <wnaks9901@gmail.com>
2026-02-04 22:34:11 +0900

    [Day 131] Daily Codekata(~26/02/04) - 복습
     116. 점 찍기(수식계산) / re (2트 완료? 한번더..? 형변환에서 좀 애먹었음)
    
    https://blog.naver.com/letterb926/224171947414

--- 116.cpp
+++ 116.cpp
@@ -1,4 +1,4 @@
-// 116. 점 찍기(수식계산) / re
+// 116. 점 찍기(수식계산) / re (2트 완료? 한번더..? 형변환에서 좀 애먹었음)
 // https://school.programmers.co.kr/learn/courses/30/lessons/140107
 #include <string>
 #include <vector>
@@ -6,6 +6,19 @@
 
 using namespace std;
 
+/* 2트 (부분정답 대부분은 형변환 문제) */
+long long solution(int k, int d) {
+    long long ans=0;
+    long long dd = (long long)d*d;  // NOTE. 미리 형변환 해야 손실 없음
+    for(int i=0; i<=d; i+=k){
+        long long ii = (long long)i*i;
+        long long res = sqrt(dd - ii) / k;
+        ans += res + 1;
+    }
+    return ans;
+}
+
+/* 1트 */
 long long solution(int k, int d) {
     // d*d >= a*a*k*k + b*b*k*k 를 만족하는 (a,b)의 개수
     long long ans = 0;
