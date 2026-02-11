81fbcdba316036bf918368e3d21347fa32a172f5
JangJuMan <wnaks9901@gmail.com>
2026-02-11 11:38:09 +0900

    [Day 134] Daily Codekata(~26/02/11) - 복습
    // 119. 숫자 카드 나누기 (gcd) / re(2트 완료)
    
    https://blog.naver.com/letterb926/224179854656

--- 119.cpp
+++ 119.cpp
@@ -1,4 +1,4 @@
-// 119. 숫자 카드 나누기 (gcd) / re
+// 119. 숫자 카드 나누기 (gcd) / re(2트 완료)
 // https://school.programmers.co.kr/learn/courses/30/lessons/135807
 #include <string>
 #include <vector>
@@ -7,31 +7,65 @@
 
 using namespace std;
 
-bool divisorCheck(const vector<int>& vec, int divisor){
-    for(int num : vec){
-        if(num % divisor == 0){
-            return false;
-        }
-    }
-    return true;
+int mygcd_recursion(int a, int b){      // 함수 호출 오버헤드
+    if(a % b == 0)
+        return b
+    return mygcd_recursion(b, a % b);
 }
 
-int solution(vector<int> arrayA, vector<int> arrayB) {
-    // array A의 모든 공약수
+int mygcd_while(int a, int b){          // 반복문으로 개선
+    while(b != 0){
+        int tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+int solution(vector<int> arrayA, vector<int> arrayB){
     int gcdA = arrayA[0];
     int gcdB = arrayB[0];
     for(int i=1; i<arrayA.size(); i++){
         gcdA = gcd(gcdA, arrayA[i]);
         gcdB = gcd(gcdB, arrayB[i]);
     }
-    
-    int ans = 0;
-    if(divisorCheck(arrayA, gcdB)){
-        ans = max(ans, gcdB);
+
+    int ansA = 0, ansB = 0;
+    for(int i=0; i<arrayA.size(); i++){
+        if(arrayB[i] % gcdA == 0) 
+            ansA = 0;
+        if(arrayA[i] % gcdB == 0) 
+            ansB = 0;
     }
-    if(divisorCheck(arrayB, gcdA)){
-        ans = max(ans, gcdA);
-    }
-    return ans;
-    
+    return max(ansA, ansB);
 }
+
+
+/* 1트 */
+// bool divisorCheck(const vector<int>& vec, int divisor){
+//     for(int num : vec){
+//         if(num % divisor == 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// int solution(vector<int> arrayA, vector<int> arrayB) {
+//     // array A의 모든 공약수
+//     int gcdA = arrayA[0];
+//     int gcdB = arrayB[0];
+//     for(int i=1; i<arrayA.size(); i++){
+//         gcdA = gcd(gcdA, arrayA[i]);
+//         gcdB = gcd(gcdB, arrayB[i]);
+//     }
+    
+//     int ans = 0;
+//     if(divisorCheck(arrayA, gcdB)){
+//         ans = max(ans, gcdB);
+//     }
+//     if(divisorCheck(arrayB, gcdA)){
+//         ans = max(ans, gcdA);
+//     }
+//     return ans;   
+// }
