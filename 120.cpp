2f41e2bb0176ee787157aae0a70972daad0c81ee
JangJuMan <wnaks9901@gmail.com>
2026-02-12 13:41:31 +0900

    [Day 135] Daily Codekata(~26/02/12) - 복습
    // 120. 멀쩡한 사각형(gcd, 공식?) / re(2트 완료, 캐스팅 주의)
    
    https://blog.naver.com/letterb926/224181336844

--- 120.cpp
+++ 120.cpp
@@ -1,9 +1,19 @@
-// 120. 멀쩡한 사각형(gcd, 공식?) / re
+// 120. 멀쩡한 사각형(gcd, 공식?) / re(2트 완료, 캐스팅 주의)
 // https://school.programmers.co.kr/learn/courses/30/lessons/62048
 #include <numeric>
 
 using namespace std;
 
+/* 2트 */
+long long solution(int w, int h){
+    long long W = (long long)w;
+    long long H = (long long)h;
+    long long total = W*H;
+    long long unusable = W + H - gcd(W, H);
+    return total - unusable;
+}
+
+/* 1트 */
 int getGCD(int a, int b){   // gcd(a, b) = gcd(b, r) / 복습용
     while(b != 0){
         int r = a % b;
