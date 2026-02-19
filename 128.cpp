c336587f3639e8be10662a464e869ecf375acd61
JangJuMan <wnaks9901@gmail.com>
2026-02-19 14:20:12 +0900

    [Day 138] Daily Codekata(~26/02/19) - 복습
    // 128. 디펜스 게임 (pq, 최적화) / 풀었지만 다시? (2트 완료)
    
    https://blog.naver.com/letterb926/224188124944

--- 128.cpp
+++ 128.cpp
@@ -1,4 +1,4 @@
-// 128. 디펜스 게임 (pq, 최적화) / 풀었지만 다시?
+// 128. 디펜스 게임 (pq, 최적화) / 풀었지만 다시? (2트 완료)
 // https://school.programmers.co.kr/learn/courses/30/lessons/142085#
 #include <string>
 #include <vector>
@@ -6,26 +6,50 @@
 
 using namespace std;
 
+/* 2트 */
 int solution(int n, int k, vector<int> enemy) {
-    if(k >= (int)enemy.size()) return enemy.size(); // unsigned int -> int
-    
-    int enemySum = 0, round;
-    priority_queue<int, vector<int>, less<int>> maxHeap;
-    
-    for(round=0; round<enemy.size(); round++){
-        enemySum += enemy[round];
-        maxHeap.emplace(enemy[round]);
+    priority_queue<int, vector<int>, less<int>> pq;
+    int ans = 0;
+    for(int i=0; i<enemy.size(); i++){
+        pq.emplace(enemy[i]);
+        n -= enemy[i];
         
-        if(enemySum > n){
-            if(k > 0){
-                enemySum -= maxHeap.top();
-                maxHeap.pop();
-                k--;
+        if(n<0){        // 병사가 부족해지면
+            if(k>0){    // 무적권 사용
+                n += pq.top();  // 가장 큰 적의 수만큼 병사 복구
+                pq.pop();
+                k--;    
             }
             else{
-                break;
+                break;          // 무적권 없으면 게임 종료
             }
         }
+        ans++;
     }
-    return round;
+    return ans;
 }
+
+/* 1트 */
+// int solution(int n, int k, vector<int> enemy) {
+//     if(k >= (int)enemy.size()) return enemy.size(); // unsigned int -> int
+    
+//     int enemySum = 0, round;
+//     priority_queue<int, vector<int>, less<int>> maxHeap;
+    
+//     for(round=0; round<enemy.size(); round++){
+//         enemySum += enemy[round];
+//         maxHeap.emplace(enemy[round]);
+        
+//         if(enemySum > n){
+//             if(k > 0){
+//                 enemySum -= maxHeap.top();
+//                 maxHeap.pop();
+//                 k--;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//     }
+//     return round;
+// }
