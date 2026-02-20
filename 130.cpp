65de90ac87142d40e0087504c1d5e7e4f21cc6a4
JangJuMan <wnaks9901@gmail.com>
2026-02-20 13:03:20 +0900

    [Day 139] Daily Codekata(~26/02/20) - 복습
    // 130. 두 원 사이의 정수 쌍 (수식, 구현) / re (2트 실패)
    
    https://blog.naver.com/letterb926/224189315394

--- 130.cpp
+++ 130.cpp
@@ -1,4 +1,4 @@
-// 130. 두 원 사이의 정수 쌍 (수식, 구현) / re
+// 130. 두 원 사이의 정수 쌍 (수식, 구현) / re (2트 실패)
 // https://school.programmers.co.kr/learn/courses/30/lessons/181187
 #include <string>
 #include <vector>
@@ -6,13 +6,36 @@
 
 using namespace std;
 
+/* 2트(1트 코드가 더 나은듯?) */
+long long solution(int r1, int r2){
+    int y_min = r1, y_max = r2;
+    long long ans = 0;
+    for(int x=1; x<=r2; x++){
+        while((long long)x*x + (long long)y_max*y_max > (long long)r2*r2){
+            y_max--;
+        }
+        
+        if(x < r1){
+            while(y_min > 0 && (long long)x*x + (long long)(y_min-1)*(y_min-1) >= (long long)r1*r1){
+                y_min--;
+            }
+        }
+        else{
+            y_min = 0;
+        }
+        ans += y_max - y_min + 1;
+    }
+    return ans * 4;
+}
+
+/* 1트 */
 long long solution(int r1, int r2) {
     long long ans = 0;
-    for(int i=1; i<=r2; i++){
-        int y_max = floor(sqrt((long long)r2*r2 - (long long)i*i));
+    for(int x=1; x<=r2; x++){
+        int y_max = floor(sqrt((long long)r2*r2 - (long long)x*x)); // x^2 + y^2 = r^2 (원의 방정식)
         int y_min = 0;
-        if(r1 >= i){
-            y_min = ceil(sqrt((long long)r1*r1 - (long long)i*i));
+        if(r1 >= x){
+            y_min = ceil(sqrt((long long)r1*r1 - (long long)x*x));
         }
         ans += (y_max - y_min + 1);
     }
