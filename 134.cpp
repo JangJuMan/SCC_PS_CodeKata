525832ba00116396ba7dd13e4a5382e571f8d071
JangJuMan <wnaks9901@gmail.com>
2026-02-25 19:23:29 +0900

    [Day 142] Daily Codekata(~26/02/25) - 복습
    // 134. 혼자하는 틱텍토(구현) / re (2트 실패)
    
    https://blog.naver.com/letterb926/224195756438

--- 134.cpp
+++ 134.cpp
@@ -1,10 +1,50 @@
-// 134. 혼자하는 틱텍토(구현) / re
+// 134. 혼자하는 틱텍토(구현) / re (2트 실패)
 // https://school.programmers.co.kr/learn/courses/30/lessons/160585#
 #include <string>
 #include <vector>
 
 using namespace std;
 
+/* 2트(실패) */
+bool winCheck(const vector<string>& board, char c){
+    for(int i=0; i<3; i++){
+        if(board[i][0] == c && board[i][1] == c && board[i][2] == c)    // 세로
+        // NOTE. 이건 순차적으로 비교하기때문에 비교 의미가 달라짐
+        // board[i][0] == board[i][1] == board[i][2] == c 
+            return true;
+        if(board[0][i] == c && board[1][i] == c && board[2][i] == c)    // 가로
+            return true;
+    }
+    
+    if(board[0][0] == c && board[1][1] == c && board[2][2] == c)
+        return true;
+    if(board[0][2] == c && board[1][1] == c && board[2][0] == c)
+        return true;
+    
+    return false;
+}
+
+int solution(vector<string> board) {
+    int cntO = 0, cntX = 0;
+    for(int i=0; i<board.size(); i++){
+        for(int j=0; j<board[0].size(); j++){
+            if(board[i][j] == 'O') cntO++;
+            else if(board[i][j] == 'X') cntX++;
+        }
+    }
+    
+    if(!(cntO == cntX + 1 || cntO == cntX)) return 0;
+    
+    bool winO = winCheck(board, 'O');
+    bool winX = winCheck(board, 'X');
+    if(winO && winX) return 0;
+    if(winO && cntO != cntX+1) return 0;
+    if(winX && cntX != cntO) return 0;
+    
+    return 1;
+}
+
+/* 1트 */
 bool win_check(const vector<string>& board, char c){
     for(int i=0; i<board.size(); i++){
         if(board[i][0] == c && board[i][1] == c && board[i][2] == c) return true;
