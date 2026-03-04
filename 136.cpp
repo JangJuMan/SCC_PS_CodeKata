e5591929775a8d56bce49aa9b23fd6ad2252146e
JangJuMan <wnaks9901@gmail.com>
2026-03-04 11:53:54 +0900

    [Day 144] Daily Codekata(~26/03/04) - 복습
     136. 요격 시스템(Greedy) / re (2트 실패)
    
    https://blog.naver.com/letterb926/224203755820

--- 136.cpp
+++ 136.cpp
@@ -1,4 +1,4 @@
-// 136. 요격 시스템(Greedy) / re
+// 136. 요격 시스템(Greedy) / re (2트 실패)
 // https://school.programmers.co.kr/learn/courses/30/lessons/181188
 #include <string>
 #include <vector>
@@ -6,6 +6,28 @@
 
 using namespace std;
 
+/* 2트(시작점 정렬 방법도 있긴 함)*/
+int solution(vector<vector<int>> targets) {
+    sort(targets.begin(), targets.end(), [](const auto& a, const auto& b){
+        if(a[0] == b[0]) return a[1] < b[1];
+        return a[0] < b[0];
+    });
+    int ans = 1, start = targets[0][0], end = targets[0][1];
+    for(int i=1; i<targets.size(); i++){
+        int s = targets[i][0], e = targets[i][1];
+        if(end <= s){
+            ans++;
+            start = s;
+            end = e;
+        }
+        else if(e < end){
+            end = e;
+        }
+    }
+    return ans;
+}
+
+/* 1트 */
 int solution(vector<vector<int>> targets) {
     sort(targets.begin(), targets.end(), [](auto& a, auto& b){
         return a[1] < b[1];
