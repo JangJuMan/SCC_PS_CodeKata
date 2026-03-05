838636b0b2548efd24b7aa1548b4603d3e788875
JangJuMan <wnaks9901@gmail.com>
2026-03-05 15:25:56 +0900

    [Day 145] Daily Codekata(~26/03/05) - 복습
    // 137. 양궁대회 (최적 dfs) / re (2트 실패)
    
    https://blog.naver.com/letterb926/224205439020

--- 137.cpp
+++ 137.cpp
@@ -1,10 +1,70 @@
-// 137. 양궁대회 (최적 dfs) / re
+// 137. 양궁대회 (최적 dfs) / re (2트 실패)
 // https://school.programmers.co.kr/learn/courses/30/lessons/92342
 #include <string>
 #include <vector>
 
 using namespace std;
 
+/* 2트 */
+constexpr int N = 11;
+
+vector<int> apeach(N, 0);
+vector<int> ryan(N, 0);
+vector<int> ans = {-1};
+int maxDiff = 0;
+
+void calcScore(){
+    int rScore = 0, aScore = 0, diff = 0;
+    for(int i=0; i<N; i++){
+        if(ryan[i] == 0 && apeach[i] == 0) continue;    // 둘 다 0점이면 아무도 점수 획득 X
+        else if(ryan[i] > apeach[i])        // ryan이 이기는 경우
+            rScore += (10-i);
+        else        // apeach가 이기는 경우
+            aScore += (10-i);
+    }
+    
+    diff = rScore - aScore;
+    if(diff > maxDiff){     // 점수차이가 가장 크게 벌어지는 상황찾기
+        maxDiff = diff;
+        ans = ryan;
+    }
+    else if(diff == maxDiff){    // 점수차이가 같으면, 낮은 점수에 더 많은 화살을 쓴 경우를 선택
+        for(int i=10; i>=0; i--){
+            if(ryan[i] > ans[i]){
+                ans = ryan;
+                break;
+            }
+            else if(ans[i] > ryan[i]){
+                break;
+            }
+            // ans[i] == ryan[i]인 경우엔 다음 점수 확인
+        }
+    }
+}
+
+void dfs(int cur, int arrows){
+    if(cur == N){
+        if(arrows > 0) ryan[10] += arrows;  // 남은 화살이 있다면 전부 0점에 쏘기 (화살 전부 써야함 + 낮은 점수 우선)
+        calcScore();
+        ryan[10] -= arrows; // 백트래킹을 위한 복구
+        return;
+    }
+    
+    if(arrows > apeach[cur]){       // ryan이 cur의 점수를 획득할 경우(apeach보다 화살 1개 더 쏨)
+        ryan[cur] = apeach[cur] + 1;
+        dfs(cur + 1, arrows - ryan[cur]);
+        ryan[cur] = 0;  // 백트래킹을 위한 복구
+    }
+    dfs(cur + 1, arrows); // ryan이 cur의 점수를 획득하지 못하는 경우(화살 안 쏨)
+}
+
+vector<int> solution(int n, vector<int> info) {
+    apeach = info;
+    dfs(0, n);
+    return ans;
+}
+
+/* 1트 */
 vector<int> apeach(11, 0);
 vector<int> ryan(11, 0);
 vector<int> ans = {-1};
