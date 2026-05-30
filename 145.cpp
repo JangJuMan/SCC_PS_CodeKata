28787a0721fe14440b54938fa247febd3b5f4534
JangJuMan <wnaks9901@gmail.com>
2026-05-30 15:14:58 +0900

    [Day 153] Daily Codekata(~26/05/30) - 복습
    145. 단어 변환 (BFS) / re (2트 성공)

--- 145.cpp
+++ 145.cpp
@@ -1,4 +1,4 @@
-// 145. 단어 변환 (BFS) / re
+// 145. 단어 변환 (BFS) / re (2트 성공)
 // https://school.programmers.co.kr/learn/courses/30/lessons/43163
 #include <string>
 #include <vector>
@@ -6,20 +6,33 @@
 
 using namespace std;
 
-bool canTransform(const string& a, const string& b){
-    int diff = 0;
+int getDiff(const string& a, const string& b){
+    int cnt = 0;
     for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]) diff++;
-        if(diff > 1) return false;
+        if(a[i] != b[i]) cnt++;
     }
-    return diff == 1;
+    return cnt;
 }
+
+bool canTransform(const string& a, const string& b){
+    if(getDiff(a, b) == 1) return true;
+    return false;
+}
+
+// bool canTransform(const string& a, const string& b){
+//     int diff = 0;
+//     for(int i=0; i<a.size(); i++){
+//         if(a[i] != b[i]) diff++;
+//         if(diff > 1) return false;
+//     }
+//     return diff == 1;
+// }
 
 int solution(string begin, string target, vector<string> words) {
     queue<pair<string, int>> q;
     vector<bool> visited(words.size(), false);
     
-    q.push({begin, 0});
+    q.emplace(begin, 0);
     
     while(!q.empty()){
         auto [curr, step] = q.front();
@@ -30,7 +43,7 @@ int solution(string begin, string target, vector<string> words) {
         for(int i=0; i<words.size(); i++){
             if(!visited[i] && canTransform(curr, words[i])){
                 visited[i] = true;
-                q.push({words[i], step+1});
+                q.emplace(words[i], step+1);
             }
         }
     }
