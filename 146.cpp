9e56f9c9d4b4cb607b01de5ce86399047f3e8439
JangJuMan <wnaks9901@gmail.com>
2026-06-01 19:31:07 +0900

    [Day 154] Daily Codekata(~26/06/01) - 복습
     146. 베스트 앨범 (unordered_map, 구현) / re.. 풀었지만, 컨테이너 활용 더 잘하기 (2트 실패)

--- 146.cpp
+++ 146.cpp
@@ -1,4 +1,4 @@
-// 146. 베스트 앨범 (unordered_map, 구현) / re.. 풀었지만, 컨테이너 활용 더 잘하기
+// 146. 베스트 앨범 (unordered_map, 구현) / re.. 풀었지만, 컨테이너 활용 더 잘하기 (2트 실패)
 // https://school.programmers.co.kr/learn/courses/30/lessons/42579
 #include <string>
 #include <vector>
@@ -8,35 +8,78 @@
 
 using namespace std;
 
-vector<int> solution(vector<string> genres, vector<int> plays){
-    unordered_map<string, int> genreTotal;
-    unordered_map<string, vector<pair<int, int>>> songs;
+/* 2트 실패 (Song 버전) */
+struct Song{
+    int id, play;
+    Song(int idIn, int playIn) : id(idIn), play(playIn) {}
+};
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> ans;
+    map<string, int> genrePlaySum;
+    map<string, vector<Song>> genreSongs;
     
-    for(int i=0; i<genres.size(); i++){     // 장르별 총 재생수 계산
-        genreTotal[genres[i]] += plays[i];
-        songs[genres[i]].push_back({i, plays[i]});
+    for(int i=0; i<genres.size(); i++){
+        genrePlaySum[genres[i]] += plays[i];
+        genreSongs[genres[i]].emplace_back(i, plays[i]);    // emplace 쓰려면 매개변수 생성자 정의 필요 (push_back은 안해도 됨)
+        // genreSongs[genres[i]].push_back({i, plays[i]});
     }
     
-    vector<pair<string, int>> genreOrder(genreTotal.begin(), genreTotal.end());     // unordered_map → vector 복사
-    sort(genreOrder.begin(), genreOrder.end(), [](const auto& a, const auto& b){    // 총 재생 수 기준으로 정렬
+    vector<pair<string, int>> sortedGenres(genrePlaySum.begin(), genrePlaySum.end());
+    sort(sortedGenres.begin(), sortedGenres.end(), [](const auto& a, const auto& b){
         return a.second > b.second;
     });
     
-    vector<int> ans;
-    for(const auto& [gen, total] : genreOrder){
-        auto& vec = songs[gen];
-        
-        sort(vec.begin(), vec.end(), [](const auto& a, const auto& b){  // 장르 내에서 재생수(높은순), 인덱스(낮은순) 정렬
-            if(a.second == b.second) return a.first < b.first;
-            return a.second > b.second;
+    for(const auto& [genre, sum] : sortedGenres){
+        auto& songs = genreSongs[genre];    // auto : vector<Song>
+        sort(songs.begin(), songs.end(), [](const auto& a, const auto& b){
+            if(a.play == b.play){
+                return a.id < b.id;
+            }
+            return a.play > b.play;
         });
         
-        for(int i=0; i<vec.size() && i<2; i++){ // 최대 2곡 선택
-            ans.push_back(vec[i].first);
+        int cnt=0;
+        for(const auto& s : songs){
+            if(cnt >= 2) break;
+            ans.emplace_back(s.id);
+            cnt++;
         }
     }
+    
     return ans;
 }
+
+/* 1트 실패 (pair 버전) */
+// vector<int> solution(vector<string> genres, vector<int> plays){
+//     unordered_map<string, int> genreTotal;
+//     unordered_map<string, vector<pair<int, int>>> songs;
+    
+//     for(int i=0; i<genres.size(); i++){     // 장르별 총 재생수 계산
+//         genreTotal[genres[i]] += plays[i];
+//         songs[genres[i]].push_back({i, plays[i]});
+//     }
+    
+//     vector<pair<string, int>> genreOrder(genreTotal.begin(), genreTotal.end());     // unordered_map → vector 복사
+//     sort(genreOrder.begin(), genreOrder.end(), [](const auto& a, const auto& b){    // 총 재생 수 기준으로 정렬
+//         return a.second > b.second;
+//     });
+    
+//     vector<int> ans;
+//     for(const auto& [gen, total] : genreOrder){
+//         auto& vec = songs[gen];
+        
+//         sort(vec.begin(), vec.end(), [](const auto& a, const auto& b){  // 장르 내에서 재생수(높은순), 인덱스(낮은순) 정렬
+//             if(a.second == b.second) return a.first < b.first;
+//             return a.second > b.second;
+//         });
+        
+//         for(int i=0; i<vec.size() && i<2; i++){ // 최대 2곡 선택
+//             ans.push_back(vec[i].first);
+//         }
+//     }
+//     return ans;
+// }
 
 /* 나의 더러운 코드.. */
 // vector<int> solution(vector<string> genres, vector<int> plays) {
